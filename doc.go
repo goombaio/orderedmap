c13d2e8583314343636067fd2db7ad9c5471eaec
Raül Pérez <repejota@gmail.com>
2018-09-19 23:17:01 +0200

    API docs

--- doc.go
+++ doc.go
@@ -15,4 +15,38 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
+/*
+Package orderedmap implements an insertion-ordered associative array, also
+called map or dictionary. Maps are an abstract data type that can hold data in
+(key, value) pairs.
+
+Associative arrays have two imporant properties. Every key can only appear
+once, and, every key can only have one value.
+
+An insertion-ordered map have the extra property that when it is iterated it
+returns their values in order they where inserted originally.
+
+Examples:
+
+	package main
+
+	import (
+		"github.com/goombaio/orderedmap"
+	)
+
+	func main() {
+		m := orderedmap.NewOrderedMap()
+		m.Put("one", "First element")
+		m.Put("two", "Second element")
+		m.Put("three", "Last element")
+
+		for _, entry := range m.Values() {
+			fmt.Println(entry)
+		}
+		// Output:
+		// First element
+		// Second element
+		// Last element
+	}
+*/
 package orderedmap
